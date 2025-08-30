8ec0043f7a83c650f5defd47335ec2355bd631b8
zero-nexora <hienkoi05@gmail.com>
2025-08-30 13:10:11 +0700

    continue

--- src/components/global/blur-page.tsx
+++ src/components/global/blur-page.tsx
@@ -5,7 +5,7 @@ interface BlurPageProps {
 export const BlurPage = ({ children }: BlurPageProps) => {
   return (
     <div
-      className="h-screen overflow-scroll backdrop-blur-[35px] dark:bg-muted/40 bg-muted/60 dark:shadow-2xl dark:shadow-black mx-auto pt-24 p-4 absolute top-0 right-0 left-0 bottom-0 z-[11]"
+      className="h-screen overflow-auto backdrop-blur-[35px] dark:bg-muted/40 bg-muted/60 dark:shadow-2xl dark:shadow-black mx-auto pt-24 p-4 absolute top-0 right-0 left-0 bottom-0 z-[11]"
       id="blur-page"
     >
       {children}
